9fc963558ca89ec0f4459b428a7f58d60e5e5f1e
victor-meza-07 <victord.meza@gmail.com>
2020-03-10 18:41:31 -0500

    updating local version

--- HumaneSociety/Program.cs
+++ HumaneSociety/Program.cs
@@ -10,23 +10,22 @@ namespace HumaneSociety
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             // PointOfEntry.Run();
             Employee employee = new Employee();
             employee.EmployeeId = 10;
             //employee.FirstName = "J";
             //Query.RunEmployeeQueries(employee, "create");
             Query.RunEmployeeQueries(employee, "delete");
-        }  
-=======
-            //PointOfEntry.Run();
-
-
-            //Laika still out here!
-            Animal humainlyTestedAnimal = Query.GetAnimalByID(7);
-
             
-        }
->>>>>>> 7162cf9f71e5d0452e734462fd26f8a9f3af0324
+            
+            //Laika still out here!
+            Animal humainlyTestedAnimal = Query.GetAnimalByID(7)
+
+
+
+        }  
+          
+            
+        
     }
 }
